67928571940574a1c65a24985c61f44ff26cb1d6
Jin Kwon <onacit@wemakeprice.com>
2020-05-15 14:46:55 +0900

    Simplify SimpleOctetConsumerAttachable

--- src/main/java/com/github/jinahya/bit/io/SimpleOctetConsumerAttachable.java
+++ src/main/java/com/github/jinahya/bit/io/SimpleOctetConsumerAttachable.java
@@ -9,27 +9,20 @@ import static java.util.Objects.requireNonNull;
 class SimpleOctetConsumerAttachable implements OctetConsumerAttachable {
 
     void consumeOctet(final int octet) {
-        octetConsumers().removeIf(v -> !v.test(octet));
+        octetConsumers.removeIf(v -> !v.test(octet));
     }
 
     @Override
     public boolean attachOctetConsumer(final IntPredicate octetConsumer) {
         requireNonNull(octetConsumer, "octetConsumer is null");
-        return octetConsumers().add(octetConsumer);
+        return octetConsumers.add(octetConsumer);
     }
 
     @Override
     public boolean detachOctetConsumer(final IntPredicate octetConsumer) {
         requireNonNull(octetConsumer, "octetConsumer is null");
-        return octetConsumers().remove(octetConsumer);
+        return octetConsumers.remove(octetConsumer);
     }
 
-    private Set<IntPredicate> octetConsumers() {
-        if (octetConsumers == null) {
-            octetConsumers = new HashSet<>();
-        }
-        return octetConsumers;
-    }
-
-    private Set<IntPredicate> octetConsumers;
+    private final Set<IntPredicate> octetConsumers = new HashSet<>();
 }
