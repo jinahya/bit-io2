a65dd9c44d16eca6f62b1d25770e04b2ee55d717
Jin Kwon <onacit@gmail.com>
2022-10-02 13:46:10 +0900

    wip

---
+++ src/main/java/com/github/jinahya/bit/io/_BitInput.java
@@ -0,0 +1,43 @@
+package com.github.jinahya.bit.io;
+
+/*-
+ * #%L
+ * bit-io2
+ * %%
+ * Copyright (C) 2020 Jinahya, Inc.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.IOException;
+
+/**
+ * An interface for reading values of an arbitrary number of bits.
+ *
+ * @author Jin Kwon &lt;jinahya_at_gmail.com&gt;
+ * @see BitOutput
+ */
+interface _BitInput {
+
+    /**
+     * Reads an {@code int} value of specified number of bits.
+     *
+     * @param unsigned a flag for indicating unsigned value; {@code true} for unsigned, {@code false} for signed.
+     * @param size     the number of bits to read; between {@code 1} and ({@value Integer#SIZE} -
+     *                 ({@code unsigned ? 1: 0})), both inclusive.
+     * @return an {@code int} value of specified {@code size}.
+     * @throws IOException if an I/O error occurs.
+     */
+    int _readInt(boolean unsigned, int size) throws IOException;
+}
